1e6339856c5b14b8ff65a754b57bb1e4456f2bd2
Dtrain112358 <dtrain11235813@protonmail.com>
2023-04-13 19:39:23 -0500

    coding challenge 1 complete

---
+++ codingChallenge1.js
@@ -0,0 +1,9 @@
+function triArea (base, height) {
+    return (base * height)/2
+}
+
+console.log(triArea(3,2)); // equals 3
+
+console.log(triArea(7,4)); // equals 14
+
+console.log(triArea(10,10)); // equals 50
